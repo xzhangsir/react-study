26709e1b7e6778778f4aa97a0ecc5a0f84dcd0eb
张鑫 <2916733574@qq.com>
2020-07-26 23:31:24 +0800

    '必须学会react'

--- react-app1/src/index.js
+++ react-app1/src/index.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+import { BrowserRouter } from 'react-router-dom'  //使用react-router路由  下面的<App/>需要用BrowserRouter组件包裹起来
 import App from './components/app'
 
-
-ReactDOM.render(( <App />), document.getElementById('root'))
+ReactDOM.render((
+  <BrowserRouter>
+  <App/>
+  </BrowserRouter>
+),document.getElementById('root'))
